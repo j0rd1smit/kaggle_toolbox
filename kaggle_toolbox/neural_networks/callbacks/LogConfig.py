a3d149ba4decd50ec5ca779f2c6c081d02f6baa0
Jordi <jordismit20000@gmail.com>
2021-05-16 17:42:48 +0200

    work with both config

--- kaggle_toolbox/neural_networks/callbacks/LogConfig.py
+++ kaggle_toolbox/neural_networks/callbacks/LogConfig.py
@@ -1,5 +1,6 @@
 from argparse import Namespace
 from pathlib import Path
+from typing import Dict, Union
 
 import pytorch_lightning as pl
 import yaml
@@ -7,12 +8,15 @@ from jsonargparse import namespace_to_dict
 from pytorch_lightning import Callback
 
 
-class LogNameSpace(Callback):
-    def __init__(self, name_space: Namespace, file_name: str = "config") -> None:
-        self.name_space = name_space
+class LogConfig(Callback):
+    def __init__(self, config: Union[Namespace, Dict], file_name: str = "config") -> None:
+        self.config = config
         self.file_name = file_name
 
     def on_fit_start(self, trainer: "pl.Trainer", pl_module: "pl.LightningModule") -> None:
+        self._log_config(trainer)
+
+    def _log_config(self, trainer: "pl.Trainer"):
         if trainer.log_dir is None:
             return
 
@@ -21,4 +25,11 @@ class LogNameSpace(Callback):
         output_path = log_dir / f"{self.file_name}.yaml"
 
         with open(output_path, "w") as f:
-            yaml.dump(namespace_to_dict(self.name_space), f)
+            config = self.config
+            if isinstance(config, Namespace):
+                config = namespace_to_dict(self.config)
+
+            yaml.dump(config, f)
+
+    def on_fit_end(self, trainer: "pl.Trainer", pl_module: "pl.LightningModule") -> None:
+        self._log_config(trainer)
